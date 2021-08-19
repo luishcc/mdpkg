3d9f31b2a8bdee69d7607225584038da7836377a
luishcc <lh.carnevale@gmail.com>
2021-08-19 16:23:34 +0200

    changing to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
- from setuptools import setup
+from setuptools import setup
 
- setup(
-   name='mdpkg',
-   version='0.0.1',
-   author='Luís H. Carnevale',
-   author_email='lh.carnevale@gmail.com',
-   packages=['mdpkg'],
-   url='https://github.com/luishcc/mdpkg',
-   license='LICENSE.txt',
-   description='For Molecular Dynamics analysis',
-   long_description=open('README.md').read(),
-   install_requires=[
+setup(
+    name='mdpkg',
+    version='0.0.1',
+    author='Luís H. Carnevale',
+    author_email='lh.carnevale@gmail.com',
+    packages=['mdpkg'],
+    url='https://github.com/luishcc/mdpkg',
+    license='LICENSE.txt',
+    description='For Molecular Dynamics analysis',
+    long_description=open('README.md').read(),
+    install_requires=[
        "numpy",
-   ],
-   python_requires='>=3.7'
-)
+    ],
+    python_requires='>=3.7'
+    )
