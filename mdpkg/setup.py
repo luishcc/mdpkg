4fd2e78781548c7016a3da5d56e66bf080e68d0b
luishcc <lh.carnevale@gmail.com>
2021-08-19 16:17:39 +0200

    changing to setup.py

---
+++ mdpkg/setup.py
@@ -0,0 +1,17 @@
+ from setuptools import setup
+
+ setup(
+   name='mdpkg',
+   version='0.0.1',
+   author='Luís H. Carnevale',
+   author_email='lh.carnevale@gmail.com',
+   packages=['mdpkg'],
+   url='https://github.com/luishcc/mdpkg',
+   license='LICENSE.txt',
+   description='For Molecular Dynamics analysis',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "numpy",
+   ],
+   python_requires='>=3.7'
+)
